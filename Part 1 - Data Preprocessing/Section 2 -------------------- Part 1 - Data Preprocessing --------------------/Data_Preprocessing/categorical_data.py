850179d51fa4293ad097da6af2829100da613243
murali0861 <murali.kummitha@gmail.com>
2018-03-04 22:59:09 +0530

    data process encoding and decoding from SKlearn labelencoder and onehotencoder

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/categorical_data.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/categorical_data.py
@@ -1,28 +1,54 @@
-# Data Preprocessing
+# Data Preprocessing Template
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import copy
 
 # Importing the dataset
+
 dataset = pd.read_csv('Data.csv')
+
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+Y = dataset.iloc[:, 3].values
 
-# Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+# ====> taking care of missing data
 
-# Encoding categorical data
-# Encoding the Independent Variable
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+# ====> using mean strategy
+ 
+mean_x = copy.deepcopy(X)
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
+imputer = imputer.fit(mean_x[:, 1:3])
+
+mean_x[:, 1:3] = imputer.transform(mean_x[:, 1:3])
+
+print(mean_x)
+
+# ====> using median strategy
+
+median_x = copy.deepcopy(X)
+median_imputer = Imputer(missing_values= "NaN", strategy= "median", axis= 0)
+median_x[:, 1:3] = median_imputer.fit_transform(median_x[:, 1:3])
+print(median_x)
+
+# ====> using mode strategy
+
+mode_x = copy.deepcopy(X)
+mode_imputer = Imputer(missing_values= "NaN", strategy= "most_frequent", axis= 0)
+mode_x[:, 1:3] = mode_imputer.fit_transform(mode_x[:, 1:3])
+print(mode_x)
+
+
+# ====> categorical data
+
+# ====> using mean
+
+labelencoder = LabelEncoder()
+mean_x[:, 0] = labelencoder.fit_transform(mean_x[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
-# Encoding the Dependent Variable
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+mean_x = onehotencoder.fit_transform(mean_x).toarray()
+
+
