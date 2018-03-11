4488669d4d3722cee10c7fd021079e9416b9766e
murali0861 <murali.kummitha@gmail.com>
2018-03-11 20:54:26 +0530

    Simple linear regression python test set vs training set prediction

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -4,15 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
-
-# Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('Salary_Data.csv')
+
+experience = dataset.iloc[:, :-1].values
+salary = dataset.iloc[:, 1].values
+
+experience_train, experience_test, salary_train, salary_test = train_test_split(experience, salary, test_size=1/3, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -21,3 +21,29 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+
+# ====> fitting simple linear regression to the training set
+regression = LinearRegression()
+regression.fit(experience_train, salary_train)
+salary_pred = regression.predict(experience_test)
+
+
+# ====> Training set plot the regression graph
+
+plt.scatter(experience_train, salary_train, color='red')
+plt.plot(experience_train, regression.predict(experience_train), color='blue')
+plt.xlabel('No.of Years')
+plt.ylabel('Salary')
+plt.title('Experience vs Salary (Training Set)')
+plt.show()
+
+
+# ====> Test set plot regression graph
+
+plt.scatter(experience_test, salary_test, color='red')
+plt.plot(experience_train, regression.predict(experience_train), color='blue')
+plt.xlabel('No.of Years')
+plt.ylabel('Salary')
+plt.title('Experience vs Salary (Test set)')
+plt.show()
