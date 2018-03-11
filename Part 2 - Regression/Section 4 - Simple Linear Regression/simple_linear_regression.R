beb2ea0144303be9ed642b95c1521085bd27bd84
murali0861 <murali.kummitha@gmail.com>
2018-03-11 22:06:22 +0530

    Simple linear regression R lang test set vs training set prediction :zap:

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.R
@@ -1,16 +1,45 @@
-# Simple Linear Regression
+# ====> Simple linear regression algorithm
 
-# Importing the dataset
+# ====> Read csv
+# install.packages('caTools')
+# ====> install ggplot2
+# install.packages('ggplot2')
+library(caTools)
+library(ggplot2)
+setwd("~/work/mlatoz/Part 2 - Regression/Section 4 - Simple Linear Regression")
 dataset = read.csv('Salary_Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
+
+# ====> splitting data into training set and test set
 set.seed(123)
 split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+
+# ====> Fitting simple linear regression to the training set
+regressor = lm(formula = Salary ~ YearsExperience, data=training_set)
+summary(regressor)
+
+salary_predict = predict(regressor, test_set)
+salary_training_set_predict = predict(regressor, training_set)
+
+# =====> Training set data plot
+ggplot() + 
+  geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary), color='red') +
+  geom_line(aes(x=training_set$YearsExperience, y=salary_training_set_predict), color='blue') + 
+  ggtitle('Years of Experience vs Salary (Training Set)') + 
+  xlab('Years of experience') +
+  ylab('Salary')
+
+# ====> Test set data plot
+
+ggplot() + 
+  geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary), color='red') + 
+  geom_line(aes(x=training_set$YearsExperience, y=salary_training_set_predict), color='blue') +
+  ggtitle('Years of Experience vs Salary (Test set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+ 
+
+
