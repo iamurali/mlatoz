b6e2960e1c749ecf0d35391b720e8bba6d2ae128
murali0861 <murali.kummitha@gmail.com>
2018-04-17 01:22:55 +0530

    lostic regression in R :pill:

--- Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.R
+++ Part 3 - Classification/Section 14 - Logistic Regression/logistic_regression.R
@@ -1,66 +1,51 @@
 # Logistic Regression
-
+setwd("~/work/mlatoz/Part 3 - Classification/Section 14 - Logistic Regression")
+# install.packages('ElemStatLearn')
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[3:5]
-
-# Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
-
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+library(ElemStatLearn)
 library(caTools)
+
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-training_set[-3] = scale(training_set[-3])
-test_set[-3] = scale(test_set[-3])
 
-# Fitting Logistic Regression to the Training set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set)
+# ====> Scaling
 
-# Predicting the Test set results
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
+
+# ===> Logistical regression.
+classifier = glm(formula = Purchased ~ ., family = binomial, data = training_set)
+
 prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
 
-# Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred > 0.5)
+test_pred = ifelse( prob_pred > 0.5, 1, 0)
 
-# Visualising the Training set results
-library(ElemStatLearn)
-set = training_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+# ====> Confusion matrix
+cm = table(test_set[, 3], test_pred)
 
-# Visualising the Test set results
-library(ElemStatLearn)
-set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
-     xlim = range(X1), ylim = range(X2))
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+# ====> Plot grapth
+
+draw_plot <- function(set, classifier, title, filename) {
+  png(filename)
+  x1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+  x2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+  grid_set = expand.grid(x1, x2)
+  colnames(grid_set) = c('Age', 'EstimatedSalary')
+  prob_pred = predict(classifier, type = 'response', newdata = grid_set)
+  y_pred = ifelse(prob_pred > 0.5, 1, 0)
+  plot(set[, -3], main = title, xlab = 'Age', ylab = 'Estimated Salary', xlim = range(x1), ylim = range(x2))
+  contour(x1, x2, matrix(as.numeric(y_pred), length(x1), length(x2)), add = TRUE)
+  points(grid_set, pch = '.', col = ifelse(y_pred == 1, 'springgreen3', 'tomato'))
+  points(set, pch = 21,bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+  dev.off()
+}
+
+
+draw_plot(training_set, classifier, 'Logistic regression (Training set)', 'r_logistic_train.png')
+draw_plot(test_set, classifier, 'Logistic regression (Test set)', 'r_logistic_test.png')
+  
